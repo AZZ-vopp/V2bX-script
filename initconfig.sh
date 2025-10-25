bab514d9ade012ca06f6e550463d4bfd8edfcc24
VŨ VĂN THÁI <64297334+AZZ-vopp@users.noreply.github.com>
2025-10-26 01:37:36 +0700

    Update initconfig.sh

--- initconfig.sh
+++ initconfig.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-# Cấu hình một bước
+# Cấu hình một lần
 
-# Kiểm tra hệ thống có IPv6 không
+# Kiểm tra hệ thống có hỗ trợ IPv6 không
 check_ipv6_support() {
     if ip -6 addr | grep -q "inet6"; then
-        echo "1"  # Có hỗ trợ IPv6
+        echo "1"  # Hỗ trợ IPv6
     else
         echo "0"  # Không hỗ trợ IPv6
     fi
 }
 
 add_node_config() {
-    echo -e "${green}Chọn loại nhân của node:${plain}"
+    echo -e "${green}Vui lòng chọn loại lõi nút:${plain}"
     echo -e "${green}1. xray${plain}"
     echo -e "${green}2. singbox${plain}"
     echo -e "${green}3. hysteria2${plain}"
-    read -rp "Nhập lựa chọn: " core_type
+    read -rp "Vui lòng nhập: " core_type
     if [ "$core_type" == "1" ]; then
         core="xray"
         core_xray=true
@@ -26,23 +26,23 @@ add_node_config() {
         core="hysteria2"
         core_hysteria2=true
     else
-        echo "Lựa chọn không hợp lệ. Vui lòng chọn 1 2 hoặc 3."
+        echo "Lựa chọn không hợp lệ. Vui lòng chọn 1 2 3."
         continue
     fi
     while true; do
-        read -rp "Nhập Node ID: " NodeID
-        # Kiểm tra NodeID là số nguyên dương
+        read -rp "Vui lòng nhập Node ID của nút: " NodeID
+        # Kiểm tra NodeID có phải là số nguyên dương không
         if [[ "$NodeID" =~ ^[0-9]+$ ]]; then
             break  # Nhập đúng, thoát vòng lặp
         else
-            echo "Lỗi: Vui lòng nhập số hợp lệ làm Node ID."
+            echo "Lỗi: Vui lòng nhập số đúng làm Node ID."
         fi
     done
 
     if [ "$core_hysteria2" = true ] && [ "$core_xray" = false ] && [ "$core_sing" = false ]; then
         NodeType="hysteria2"
     else
-        echo -e "${yellow}Chọn giao thức truyền của node:${plain}"
+        echo -e "${yellow}Vui lòng chọn giao thức truyền tải nút:${plain}"
         echo -e "${green}1. Shadowsocks${plain}"
         echo -e "${green}2. Vless${plain}"
         echo -e "${green}3. Vmess${plain}"
@@ -58,7 +58,7 @@ add_node_config() {
             echo -e "${green}7. Tuic${plain}"
             echo -e "${green}8. AnyTLS${plain}"
         fi
-        read -rp "Nhập lựa chọn: " NodeType
+        read -rp "Vui lòng nhập: " NodeType
         case "$NodeType" in
             1 ) NodeType="shadowsocks" ;;
             2 ) NodeType="vless" ;;
@@ -73,32 +73,32 @@ add_node_config() {
     fi
     fastopen=true
     if [ "$NodeType" == "vless" ]; then
-        read -rp "Có phải là node reality không? (y/n): " isreality
+        read -rp "Vui lòng chọn có phải nút reality không? (y/n) " isreality
     elif [ "$NodeType" == "hysteria" ] || [ "$NodeType" == "hysteria2" ] || [ "$NodeType" == "tuic" ] || [ "$NodeType" == "anytls" ]; then
         fastopen=false
         istls="y"
     fi
 
     if [[ "$isreality" != "y" && "$isreality" != "Y" &&  "$istls" != "y" ]]; then
-        read -rp "Có muốn cấu hình TLS không? (y/n): " istls
+        read -rp "Vui lòng chọn có cấu hình TLS không? (y/n) " istls
     fi
 
     certmode="none"
     certdomain="example.com"
     if [[ "$isreality" != "y" && "$isreality" != "Y" && ( "$istls" == "y" || "$istls" == "Y" ) ]]; then
-        echo -e "${yellow}Chọn chế độ cấp chứng chỉ:${plain}"
-        echo -e "${green}1. Tự động http (domain đã trỏ chính xác)${plain}"
-        echo -e "${green}2. Tự động dns (phải điền API của nhà cung cấp domain)${plain}"
-        echo -e "${green}3. Tự ký hoặc đã có file chứng chỉ${plain}"
-        read -rp "Nhập lựa chọn: " certmode
+        echo -e "${yellow}Vui lòng chọn chế độ xin chứng chỉ:${plain}"
+        echo -e "${green}1. Chế độ http tự động xin, tên miền nút đã được phân giải đúng${plain}"
+        echo -e "${green}2. Chế độ dns tự động xin, cần điền đúng tham số API nhà cung cấp tên miền${plain}"
+        echo -e "${green}3. Chế độ self, tự ký chứng chỉ hoặc cung cấp file chứng chỉ có sẵn${plain}"
+        read -rp "Vui lòng nhập: " certmode
         case "$certmode" in
             1 ) certmode="http" ;;
             2 ) certmode="dns" ;;
             3 ) certmode="self" ;;
         esac
-        read -rp "Nhập domain chứng chỉ cho node (ví dụ: example.com): " certdomain
+        read -rp "Vui lòng nhập tên miền chứng chỉ nút (example.com): " certdomain
         if [ "$certmode" != "http" ]; then
-            echo -e "${red}Vui lòng tự sửa file cấu hình rồi khởi động lại V2bX!${plain}"
+            echo -e "${red}Vui lòng chỉnh sửa file cấu hình thủ công sau đó khởi động lại V2bX!${plain}"
         fi
     fi
     ipv6_support=$(check_ipv6_support)
@@ -203,14 +203,14 @@ EOF
 }
 
 generate_config_file() {
-    echo -e "${yellow}Trình hướng dẫn tạo file cấu hình V2bX${plain}"
-    echo -e "${red}Vui lòng đọc kỹ các lưu ý sau:${plain}"
-    echo -e "${red}1. Tính năng này đang thử nghiệm${plain}"
-    echo -e "${red}2. File cấu hình sẽ lưu vào /etc/V2bX/config.json${plain}"
-    echo -e "${red}3. File cấu hình cũ sẽ được lưu thành /etc/V2bX/config.json.bak${plain}"
+    echo -e "${yellow}Hướng dẫn tạo file cấu hình V2bX${plain}"
+    echo -e "${red}Vui lòng đọc các lưu ý sau:${plain}"
+    echo -e "${red}1. Hiện tại tính năng này đang trong giai đoạn thử nghiệm${plain}"
+    echo -e "${red}2. File cấu hình được tạo sẽ lưu vào /etc/V2bX/config.json${plain}"
+    echo -e "${red}3. File cấu hình gốc sẽ lưu vào /etc/V2bX/config.json.bak${plain}"
     echo -e "${red}4. Hiện tại chỉ hỗ trợ một phần TLS${plain}"
-    echo -e "${red}5. File cấu hình tạo ra sẽ có audit mặc định, bạn chắc chắn muốn tiếp tục? (y/n)${plain}"
-    read -rp "Nhập lựa chọn: " continue_prompt
+    echo -e "${red}5. File cấu hình được tạo bằng tính năng này sẽ có sẵn kiểm toán, xác nhận tiếp tục? (y/n)${plain}"
+    read -rp "Vui lòng nhập: " continue_prompt
     if [[ "$continue_prompt" =~ ^[Nn][Oo]? ]]; then
         exit 0
     fi
@@ -225,30 +225,285 @@ generate_config_file() {
     
     while true; do
         if [ "$first_node" = true ]; then
-            read -rp "Nhập địa chỉ trang quản trị (https://example.com): " ApiHost
-            read -rp "Nhập API Key: " ApiKey
-            read -rp "Có muốn cố định API Key và trang quản trị? (y/n): " fixed_api
+            read -rp "Vui lòng nhập địa chỉ trang web sân bay (https://example.com): " ApiHost
+            read -rp "Vui lòng nhập API Key kết nối bảng điều khiển: " ApiKey
+            read -rp "Có thiết lập địa chỉ trang web sân bay và API Key cố định không? (y/n) " fixed_api
             if [ "$fixed_api" = "y" ] || [ "$fixed_api" = "Y" ]; then
                 fixed_api_info=true
-                echo -e "${red}Cố định địa chỉ thành công${plain}"
+                echo -e "${red}Đã cố định địa chỉ thành công${plain}"
             fi
             first_node=false
             add_node_config
         else
-            read -rp "Tiếp tục thêm cấu hình node? (Enter để tiếp tục, nhập n hoặc no để thoát): " continue_adding_node
+            read -rp "Có tiếp tục thêm cấu hình nút không? (Enter để tiếp tục, nhập n hoặc no để thoát) " continue_adding_node
             if [[ "$continue_adding_node" =~ ^[Nn][Oo]? ]]; then
                 break
             elif [ "$fixed_api_info" = false ]; then
-                read -rp "Nhập địa chỉ trang quản trị (https://example.com): " ApiHost
-                read -rp "Nhập API Key: " ApiKey
+                read -rp "Vui lòng nhập địa chỉ trang web sân bay (https://example.com): " ApiHost
+                read -rp "Vui lòng nhập API Key kết nối bảng điều khiển: " ApiKey
             fi
             add_node_config
         fi
     done
 
-    # Các phần bên dưới giữ nguyên (không cần dịch vì là cấu hình JSON...)
-    # Kết thúc script như cũ
+    # Khởi tạo mảng cấu hình lõi
+    cores_config="["
 
-    echo -e "${green}Tạo file cấu hình V2bX hoàn tất, đang khởi động lại dịch vụ${plain}"
+    # Kiểm tra và thêm cấu hình lõi xray
+    if [ "$core_xray" = true ]; then
+        cores_config+="
+    {
+        \"Type\": \"xray\",
+        \"Log\": {
+            \"Level\": \"error\",
+            \"ErrorPath\": \"/etc/V2bX/error.log\"
+        },
+        \"OutboundConfigPath\": \"/etc/V2bX/custom_outbound.json\",
+        \"RouteConfigPath\": \"/etc/V2bX/route.json\"
+    },"
+    fi
+
+    # Kiểm tra và thêm cấu hình lõi sing
+    if [ "$core_sing" = true ]; then
+        cores_config+="
+    {
+        \"Type\": \"sing\",
+        \"Log\": {
+            \"Level\": \"error\",
+            \"Timestamp\": true
+        },
+        \"NTP\": {
+            \"Enable\": false,
+            \"Server\": \"time.apple.com\",
+            \"ServerPort\": 0
+        },
+        \"OriginalPath\": \"/etc/V2bX/sing_origin.json\"
+    },"
+    fi
+
+    # Kiểm tra và thêm cấu hình lõi hysteria2
+    if [ "$core_hysteria2" = true ]; then
+        cores_config+="
+    {
+        \"Type\": \"hysteria2\",
+        \"Log\": {
+            \"Level\": \"error\"
+        }
+    },"
+    fi
+
+    # Xóa dấu phẩy cuối cùng và đóng mảng
+    cores_config+="]"
+    cores_config=$(echo "$cores_config" | sed 's/},]$/}]/')
+
+    # Chuyển đến thư mục file cấu hình
+    cd /etc/V2bX
+    
+    # Sao lưu file cấu hình cũ
+    mv config.json config.json.bak
+    nodes_config_str="${nodes_config[*]}"
+    formatted_nodes_config="${nodes_config_str%,}"
+
+    # Tạo file config.json
+    cat <<EOF > /etc/V2bX/config.json
+{
+    "Log": {
+        "Level": "error",
+        "Output": ""
+    },
+    "Cores": $cores_config,
+    "Nodes": [$formatted_nodes_config]
+}
+EOF
+    
+    # Tạo file custom_outbound.json
+    cat <<EOF > /etc/V2bX/custom_outbound.json
+[
+    {
+        "tag": "IPv4_out",
+        "protocol": "freedom",
+        "settings": {
+            "domainStrategy": "UseIPv4v6"
+        }
+    },
+    {
+        "tag": "IPv6_out",
+        "protocol": "freedom",
+        "settings": {
+            "domainStrategy": "UseIPv6"
+        }
+    },
+    {
+        "protocol": "blackhole",
+        "tag": "block"
+    }
+]
+EOF
+    
+    # Tạo file route.json
+    cat <<EOF > /etc/V2bX/route.json
+{
+    "domainStrategy": "AsIs",
+    "rules": [
+        {
+            "outboundTag": "block",
+            "ip": [
+                "geoip:private"
+            ]
+        },
+        {
+            "outboundTag": "block",
+            "domain": [
+                "regexp:(api|ps|sv|offnavi|newvector|ulog.imap|newloc)(.map|).(baidu|n.shifen).com",
+                "regexp:(.+.|^)(360|so).(cn|com)",
+                "regexp:(Subject|HELO|SMTP)",
+                "regexp:(torrent|.torrent|peer_id=|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=)",
+                "regexp:(^.@)(guerrillamail|guerrillamailblock|sharklasers|grr|pokemail|spam4|bccto|chacuo|027168).(info|biz|com|de|net|org|me|la)",
+                "regexp:(.?)(xunlei|sandai|Thunder|XLLiveUD)(.)",
+                "regexp:(..||)(dafahao|mingjinglive|botanwang|minghui|dongtaiwang|falunaz|epochtimes|ntdtv|falundafa|falungong|wujieliulan|zhengjian).(org|com|net)",
+                "regexp:(ed2k|.torrent|peer_id=|announce|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=|magnet:|xunlei|sandai|Thunder|XLLiveUD|bt_key)",
+                "regexp:(.+.|^)(360).(cn|com|net)",
+                "regexp:(.*.||)(guanjia.qq.com|qqpcmgr|QQPCMGR)",
+                "regexp:(.*.||)(rising|kingsoft|duba|xindubawukong|jinshanduba).(com|net|org)",
+                "regexp:(.*.||)(netvigator|torproject).(com|cn|net|org)",
+                "regexp:(..||)(visa|mycard|gash|beanfun|bank).",
+                "regexp:(.*.||)(gov|12377|12315|talk.news.pts.org|creaders|zhuichaguoji|efcc.org|cyberpolice|aboluowang|tuidang|epochtimes|zhengjian|110.qq|mingjingnews|inmediahk|xinsheng|breakgfw|chengmingmag|jinpianwang|qi-gong|mhradio|edoors|renminbao|soundofhope|xizang-zhiye|bannedbook|ntdtv|12321|secretchina|dajiyuan|boxun|chinadigitaltimes|dwnews|huaglad|oneplusnews|epochweekly|cn.rfi).(cn|com|org|net|club|net|fr|tw|hk|eu|info|me)",
+                "regexp:(.*.||)(miaozhen|cnzz|talkingdata|umeng).(cn|com)",
+                "regexp:(.*.||)(mycard).(com|tw)",
+                "regexp:(.*.||)(gash).(com|tw)",
+                "regexp:(.bank.)",
+                "regexp:(.*.||)(pincong).(rocks)",
+                "regexp:(.*.||)(taobao).(com)",
+                "regexp:(.*.||)(laomoe|jiyou|ssss|lolicp|vv1234|0z|4321q|868123|ksweb|mm126).(com|cloud|fun|cn|gs|xyz|cc)",
+                "regexp:(flows|miaoko).(pages).(dev)"
+            ]
+        },
+        {
+            "outboundTag": "block",
+            "ip": [
+                "127.0.0.1/32",
+                "10.0.0.0/8",
+                "fc00::/7",
+                "fe80::/10",
+                "172.16.0.0/12"
+            ]
+        },
+        {
+            "outboundTag": "block",
+            "protocol": [
+                "bittorrent"
+            ]
+        },
+        {
+            "outboundTag": "IPv4_out",
+            "network": "udp,tcp"
+        }
+    ]
+}
+EOF
+    ipv6_support=$(check_ipv6_support)
+    dnsstrategy="ipv4_only"
+    if [ "$ipv6_support" -eq 1 ]; then
+        dnsstrategy="prefer_ipv4"
+    fi
+    # Tạo file sing_origin.json
+    cat <<EOF > /etc/V2bX/sing_origin.json
+{
+  "dns": {
+    "servers": [
+      {
+        "tag": "cf",
+        "address": "1.1.1.1"
+      }
+    ],
+    "strategy": "$dnsstrategy"
+  },
+  "outbounds": [
+    {
+      "tag": "direct",
+      "type": "direct",
+      "domain_resolver": {
+        "server": "cf",
+        "strategy": "$dnsstrategy"
+      }
+    },
+    {
+      "type": "block",
+      "tag": "block"
+    }
+  ],
+  "route": {
+    "rules": [
+      {
+        "ip_is_private": true,
+        "outbound": "block"
+      },
+      {
+        "domain_regex": [
+            "(api|ps|sv|offnavi|newvector|ulog.imap|newloc)(.map|).(baidu|n.shifen).com",
+            "(.+.|^)(360|so).(cn|com)",
+            "(Subject|HELO|SMTP)",
+            "(torrent|.torrent|peer_id=|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=)",
+            "(^.@)(guerrillamail|guerrillamailblock|sharklasers|grr|pokemail|spam4|bccto|chacuo|027168).(info|biz|com|de|net|org|me|la)",
+            "(.?)(xunlei|sandai|Thunder|XLLiveUD)(.)",
+            "(..||)(dafahao|mingjinglive|botanwang|minghui|dongtaiwang|falunaz|epochtimes|ntdtv|falundafa|falungong|wujieliulan|zhengjian).(org|com|net)",
+            "(ed2k|.torrent|peer_id=|announce|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=|magnet:|xunlei|sandai|Thunder|XLLiveUD|bt_key)",
+            "(.+.|^)(360).(cn|com|net)",
+            "(.*.||)(guanjia.qq.com|qqpcmgr|QQPCMGR)",
+            "(.*.||)(rising|kingsoft|duba|xindubawukong|jinshanduba).(com|net|org)",
+            "(.*.||)(netvigator|torproject).(com|cn|net|org)",
+            "(..||)(visa|mycard|gash|beanfun|bank).",
+            "(.*.||)(gov|12377|12315|talk.news.pts.org|creaders|zhuichaguoji|efcc.org|cyberpolice|aboluowang|tuidang|epochtimes|zhengjian|110.qq|mingjingnews|inmediahk|xinsheng|breakgfw|chengmingmag|jinpianwang|qi-gong|mhradio|edoors|renminbao|soundofhope|xizang-zhiye|bannedbook|ntdtv|12321|secretchina|dajiyuan|boxun|chinadigitaltimes|dwnews|huaglad|oneplusnews|epochweekly|cn.rfi).(cn|com|org|net|club|net|fr|tw|hk|eu|info|me)",
+            "(.*.||)(miaozhen|cnzz|talkingdata|umeng).(cn|com)",
+            "(.*.||)(mycard).(com|tw)",
+            "(.*.||)(gash).(com|tw)",
+            "(.bank.)",
+            "(.*.||)(pincong).(rocks)",
+            "(.*.||)(taobao).(com)",
+            "(.*.||)(laomoe|jiyou|ssss|lolicp|vv1234|0z|4321q|868123|ksweb|mm126).(com|cloud|fun|cn|gs|xyz|cc)",
+            "(flows|miaoko).(pages).(dev)"
+        ],
+        "outbound": "block"
+      },
+      {
+        "outbound": "direct",
+        "network": [
+          "udp","tcp"
+        ]
+      }
+    ]
+  },
+  "experimental": {
+    "cache_file": {
+      "enabled": true
+    }
+  }
+}
+EOF
+
+    # Tạo file hy2config.yaml           
+    cat <<EOF > /etc/V2bX/hy2config.yaml
+quic:
+  initStreamReceiveWindow: 8388608
+  maxStreamReceiveWindow: 8388608
+  initConnReceiveWindow: 20971520
+  maxConnReceiveWindow: 20971520
+  maxIdleTimeout: 30s
+  maxIncomingStreams: 1024
+  disablePathMTUDiscovery: false
+ignoreClientBandwidth: false
+disableUDP: false
+udpIdleTimeout: 60s
+resolver:
+  type: system
+acl:
+  inline:
+    - direct(geosite:google)
+    - reject(geosite:cn)
+    - reject(geoip:cn)
+masquerade:
+  type: 404
+EOF
+    echo -e "${green}Hoàn thành tạo file cấu hình V2bX, đang khởi động lại dịch vụ${plain}"
     v2bx restart
 }
